a08bcc2c4ae78e23e5d7a9c9ebb76eda1eb4d0f6
soyongkim <thdyd324@gmail.com>
2020-02-20 14:59:28 +0900

    update 200220

---
+++ mafiaGoBin/view/build/precache-manifest.d082106eeb8f7dc4ac24c3ba67fbfb9c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561831bca9fa580b2e47f3d2b2b05825",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d072738d50149fa4998",
+    "url": "/static/css/main.65fb7180.chunk.css"
+  },
+  {
+    "revision": "bc578a49f480d5e0d2e1",
+    "url": "/static/js/2.69827b47.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.69827b47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d072738d50149fa4998",
+    "url": "/static/js/main.6fb9cfc7.chunk.js"
+  },
+  {
+    "revision": "965a5ad18bd6bf1b9f16",
+    "url": "/static/js/runtime-main.7f195587.js"
+  },
+  {
+    "revision": "fc9c468c0d1b44302f14c467013ee263",
+    "url": "/static/media/audio.fc9c468c.mp3"
+  },
+  {
+    "revision": "2d405717bd7b445e18db46ca31ee4d31",
+    "url": "/static/media/death.2d405717.mp3"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "776881f20253bff954ba5f06bd27902b",
+    "url": "/static/media/moon.776881f2.svg"
+  },
+  {
+    "revision": "bd1c28c6449cf2dd98b2c9c2c7b45445",
+    "url": "/static/media/repairing-service.bd1c28c6.svg"
+  }
+]);
